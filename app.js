5ea80f81aa3d04ec396f977ff4b6f095339485c8
ryan2414 <jackpar@nate.com>
2022-12-19 21:11:37 +0900

    study  Conditionals 1

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-const calculator = {
-  plus: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  devided: function (a, b) {
-    return a / b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  },
-};
+const age = parseInt(prompt("How old are you?"));
 
-const plus = calculator.plus(5, 3);
-const minus = calculator.minus(10, plus);
-const times = calculator.times(plus, minus);
+console.log(age, typeof age);
