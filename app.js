eeca12c41aadd74062edf3052678f253303b13e8
ryan2414 <jackpar@nate.com>
2022-12-20 22:23:58 +0900

    Study Events part 2

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const title = document.querySelector("div.hello:first-child h1");
 
 console.dir(title);
 
-function OnClickTitle() {
+function handleTitleClick() {
   console.log("On Title Click");
   title.style.color = "blue";
 }
 
-title.addEventListener("click", OnClickTitle);
+function handleMouseEnter() {
+  title.innerText = "Mouse is Here!";
+}
+
+function handleMouseOver() {
+  title.innerText = "Mouse is Gone!";
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseOver);
