f127f43a6665019d20b87c72939d72182e94a602
ryan2414 <jackpar@nate.com>
2022-12-22 21:09:22 +0900

    Study  classList & toggle

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ const h1 = document.querySelector("div.hello:first-child h1");
 //console.dir(title);
 
 function handleTitleClick() {
-  const clicked = "active";
-  if (h1.className === clicked) {
-    h1.className = "";
-  } else {
-    h1.className = clicked;
-  }
+  h1.classList.toggle("active");
 }
 
 h1.addEventListener("click", handleTitleClick);
