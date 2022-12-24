75e9287714c375beeebb5217df232ceef0f3843f
ryan2414 <jackpar@nate.com>
2022-12-25 01:09:18 +0900

    study Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -6,6 +6,15 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  paintToDo(newToDo);
+}
+
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newToDo;
+  toDoList.appendChild(li);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
